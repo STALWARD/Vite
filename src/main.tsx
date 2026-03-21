5d6cab37ffbe6d27286fc47a7da711ece77f1b4d
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-03-22 00:33:03 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import './index.css';
 import App from './App'; // Import the new App component
 
