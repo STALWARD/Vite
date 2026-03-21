c7e13c06944b3fad42845e956b2a3ea1740719f9
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-03-22 01:09:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite' // 1. Import the Tailwind plugin
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Add it to the plugins list
+    tailwindcss(),
   ],
+  build: {
+    // 1. Increases the warning limit if you have large assets
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        // 2. Automatically splits node_modules into a separate 'vendor' file
+        // This allows the browser to cache libraries separately from your code
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
