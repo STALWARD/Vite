f4825cd60a56e32b0d2724ce26d75ee3f9fef016
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-03-21 14:13:32 +0530

    Update Intro.tsx

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -52,7 +52,7 @@ const Intro: React.FC = () => {
       <div className="h-screen w-screen" id="clip">
         <div className="mask-clip-path about-image">
           <img
-            src="img/about.webp"
+            src="img/intro.webp"
             alt="bgImage"
             className="absolute left-0 top-0 size-full object-cover"
           />
