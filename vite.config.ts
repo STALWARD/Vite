6ae8726edf790aae456a44c74d3c9d8a49bc0203
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-03-22 11:44:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite' // 1. Import the Tailwind plugin
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Split big libraries into their own files
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
-              return '@framework'; 
-            }
-            // Let other libraries stay separate or grouped by package name
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(), // 2. Add it to the plugins list
+  ],
 })
