400184765abae8936c610df319b7778afe2a0429
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-03-25 21:46:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite' // 1. Import the Tailwind plugin
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Add it to the plugins list
+    tailwindcss(),
   ],
+  build: {
+    // 1. Splits CSS into smaller files based on JS chunks
+    cssCodeSplit: true, 
+    rollupOptions: {
+      output: {
+        // 2. Manual Chunking: Moves React & Tailwind internals 
+        // into a separate 'vendor' file to keep your 'index.js' small.
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
