85e0c6414c97c50d2e2d61ae735e005fae8d09ec
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-04-07 10:03:49 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const socialLinks = [
 
 const navLinks = [
   { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
+  { href: "/about-us", label: "About" },
   { href: "/services", label: "Services" },
   { href: "/blog", label: "Blog" },
   { href: "/contact", label: "Contact" },
