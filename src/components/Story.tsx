7041b0f37bb37172490b83b14f324cf0e5e22716
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-04-09 22:55:33 +0530

    Update Story.tsx

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,11 +1,12 @@
 import gsap from "gsap";
 import { useRef } from "react";
+import type { MouseEvent } from "react";
 import AnimatedTitle from "./AnimatedTitle";
 
 const Story: React.FC = () => {
   const frameRef = useRef<HTMLImageElement>(null);
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLImageElement>) => {
+  const handleMouseMove = (e: MouseEvent<HTMLImageElement>) => {
     const { clientX, clientY } = e;
     const element = frameRef.current;
     if (!element) return;
@@ -42,27 +43,49 @@ const Story: React.FC = () => {
   };
 
   return (
-    <div id="story" className="min-h-screen w-screen bg-black text-blue-50">
-      <div className="flex flex-col items-center py-10 pb-24 w-full h-full">
-        <div className="relative w-full h-full">
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <div className="relative size-full">
           {/* Image container */}
-          <div className="relative w-full h-full">
-            <img
-              ref={frameRef}
-              onMouseMove={handleMouseMove}
-              onMouseLeave={handleMouseLeave}
-              onMouseUp={handleMouseLeave}
-              onMouseEnter={handleMouseLeave}
-              src="/img/satyendra-large.webp"
-              alt="entrance"
-              className="w-full h-full object-cover object-center"
-            />
+          <div className="story-img-container relative">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  src="/img/satyendra-large.webp"
+                  alt="entrance.webp"
+                  className="size-full object-cover object-center"
+                />
+              </div>
+            </div>
 
             {/* Overlapping text at top with spacing */}
             <AnimatedTitle
               title="kaulbhaskar guru ji a hidden master"
-              containerClass="absolute top-5 left-0 w-full flex justify-center text-white text-3xl font-bold mix-blend-difference z-20 pointer-events-none"
+              containerClass="absolute top-5 left-0 w-full flex items-start justify-center text-white text-3xl font-bold mix-blend-difference z-20 pointer-events-none"
             />
+
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite in="SourceGraphic" in2="flt_tag" operator="atop" />
+                </filter>
+              </defs>
+            </svg>
           </div>
         </div>
 
