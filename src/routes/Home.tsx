beb1fa80516e8ccca690970d291a6f388214d1da
STALWARD <94008690+STALWARD@users.noreply.github.com>
2026-04-10 19:07:26 +0530

    Update Home.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from "react";
 import Hero from "../components/Hero"; 
 import Intro from "../components/Intro"; 
 import SEO from "../components/SEO"; 
-
+import StatsComponent from "../components/StatsComponent";
 
 // Lazy load the rest
 const Feature = lazy(() => import("../components/Feature"));
@@ -61,6 +61,7 @@ const Home: React.FC = () => {
         <Gallery />
         <Mudra />
         <FAQ />
+        <StatsComponent />
         <Story />
         <Testimonial />
         <Mentor />
